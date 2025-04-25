3ded8c50e3fee7c77f793ac77c81b7a6ffe7d486
byar <byarotsky@gmail.com>
2025-04-25 23:18:07 +0200

    working connection to az db

--- DataLoader/Program.cs
+++ DataLoader/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Data.SqlClient;
+
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+await using var dbConnection = new SqlConnection(connectionString);
+await using var dbCommand = new SqlCommand("SELECT COUNT(*) FROM dbo.NewTable", dbConnection);
+await dbConnection.OpenAsync();
+var result = await dbCommand.ExecuteScalarAsync();
+Console.WriteLine(result);
