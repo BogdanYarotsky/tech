51431b58270f26b3ccd7694fa0b59e3627bd0878
byar <byarotsky@gmail.com>
2025-05-04 21:55:23 +0200

    add fluent migrator

--- ETL/Database/Migrator.cs
+++ ETL/Database/Migrator.cs
@@ -1,8 +1,22 @@
 
+using Microsoft.Extensions.DependencyInjection;
+using FluentMigrator.Runner;
+
 public static class Migrator
 {
     internal static void EnsureDatabaseInGoodShape(string connectionString, CancellationToken token)
     {
-        // throw new NotImplementedException();
+        using var provider = new ServiceCollection()
+            .AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb
+                .AddSqlServer()
+                .WithGlobalConnectionString(connectionString)
+                .ScanIn(typeof(Migrator).Assembly).For.All())
+            .AddLogging(lb => lb.AddFluentMigratorConsole())
+            .BuildServiceProvider();
+
+        using var scope = provider.CreateScope();
+        var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+        runner.MigrateUp();
     }
 }
