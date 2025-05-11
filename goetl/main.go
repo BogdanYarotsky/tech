56c80bffc187c7a90875d2b3f2e0069c99dc2c0e
byar <byarotsky@gmail.com>
2025-05-11 06:38:37 +0200

    working go concurrent producer/cosnumer

--- goetl/main.go
+++ goetl/main.go
@@ -2,34 +2,112 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"io"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type Report struct {
-	Salary      int
+	Country     string
 	YearsCoding int
+	Salary      int
 	Tags        []Tag
 }
 
 type Tag struct {
-	Type TagType
 	Name string
+	Type TagType
 }
 
 type TagType int
 
 const (
 	None TagType = iota
+	DevType
 	Language
+	Database
+	Platform
+	WebFramework
+	MiscTech
+	Tools
+	CollabTools
 )
 
+type TagColumn struct {
+	Name string
+	Type TagType
+}
+
+var tagsColumns = [...]TagColumn{
+	{"DevType", DevType},
+	{"LanguageHaveWorkedWith", Language},
+	{"DatabaseHaveWorkedWith", Database},
+	{"PlatformHaveWorkedWith", Platform},
+	{"WebframeHaveWorkedWith", WebFramework},
+	{"MiscTechHaveWorkedWith", MiscTech},
+	{"ToolsTechHaveWorkedWith", Tools},
+	{"NEWCollabToolsHaveWorkedWith", CollabTools},
+}
+
+const (
+	MinValidSalary = 500
+	DevPrefix      = "I am a dev"
+	FullTime       = "full-time"
+	NotAvailable   = "NA"
+)
+
+var csvFiles = [...]string{
+	"../ETL/surveys/2021.csv",
+	"../ETL/surveys/2022.csv",
+	"../ETL/surveys/2023.csv",
+	"../ETL/surveys/2024.csv",
+}
+
 func main() {
-	file, err := os.Open("../ETL/surveys/2021.csv")
+	ch := make(chan Report)
+
+	var wg sync.WaitGroup
+	wg.Add(len(csvFiles))
+
+	for _, f := range csvFiles {
+		go func(filename string) {
+			defer wg.Done()
+			parseReports(f, ch)
+		}(f)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	var reports []Report
+	for r := range ch {
+		reports = append(reports, r)
+	}
+
+	goTag := Tag{"Go", Language}
+
+	for _, r := range reports {
+		if slices.Contains(r.Tags, goTag) {
+			// goreports = append(goreports, r)
+		}
+	}
+
+	println(len(reports))
+
+	// sort.Slice(goreports, func(i, j int) bool {
+	// 	return goreports[i].Salary < goreports[j].Salary
+	// })
+
+}
+
+func parseReports(csvPath string, ch chan<- Report) {
+	file, err := os.Open(csvPath)
 	check(err, "Cannot open CSV file")
 	defer file.Close()
 
@@ -42,7 +120,6 @@ func main() {
 		columns[col] = i
 	}
 
-	var reports []Report
 	for {
 		row, err := reader.Read()
 		if err == io.EOF {
@@ -50,16 +127,16 @@ func main() {
 		}
 		check(err, "Can't read row")
 
-		if !strings.HasPrefix(row[columns["MainBranch"]], "I am a dev") {
+		if !strings.HasPrefix(row[columns["MainBranch"]], DevPrefix) {
 			continue
 		}
 
-		if !strings.HasSuffix(row[columns["Employment"]], "full-time") {
+		if !strings.HasSuffix(row[columns["Employment"]], FullTime) {
 			continue
 		}
 
 		salary, err := strconv.Atoi(row[columns["ConvertedCompYearly"]])
-		if err != nil || salary < 500 {
+		if err != nil || salary < MinValidSalary {
 			continue
 		}
 
@@ -79,17 +156,37 @@ func main() {
 			}
 		}
 
-		report := Report{
-			Salary:      salary,
-			YearsCoding: yearsCoding,
-			Tags:        []Tag{},
+		country := row[columns["Country"]]
+		switch country {
+		case "Republic of Korea":
+			country = "South Korea"
+		case "The former Yugoslav Republic of Macedonia":
+			country = "Republic of North Macedonia"
 		}
 
-		fmt.Printf("%+v\n", report)
-		reports = append(reports, report)
-	}
+		var tags []Tag
+		for _, col := range tagsColumns {
+			tagsString := row[columns[col.Name]]
+			if tagsString == NotAvailable {
+				continue
+			}
+			parts := strings.Split(tagsString, ";")
+			for _, tagName := range parts {
+				tags = append(tags, Tag{
+					Name: tagName,
+					Type: col.Type,
+				})
+			}
+		}
 
-	println(len(reports))
+		report := Report{
+			YearsCoding: yearsCoding,
+			Salary:      salary,
+			Tags:        tags,
+		}
+
+		ch <- report
+	}
 }
 
 func check(err error, message string) {
